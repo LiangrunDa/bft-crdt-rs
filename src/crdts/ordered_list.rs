6031c6187b64b404f8f840108dd8e8cd18c7afa6
Liangrun Da <liangrun.da@tum.de>
2024-12-08 23:03:23 +0100

    implement rga

--- src/crdts/ordered_list.rs
+++ src/crdts/ordered_list.rs
@@ -15,7 +15,7 @@ impl<I: PartialEq + Eq + Hash + Clone + PartialOrd, V: PartialEq + Eq + Hash + C
         }
     }
 
-    pub fn insert(&mut self, id: I, value: V, after: Option<I>) -> Option<()> {
+    pub fn insert_by_id(&mut self, id: I, value: V, after: Option<I>) -> Option<()> {
         let to_insert = (id, value, false);
         let mut cursor = self.elements.cursor_mut();
         cursor.move_next();
@@ -54,7 +54,7 @@ impl<I: PartialEq + Eq + Hash + Clone + PartialOrd, V: PartialEq + Eq + Hash + C
         }
     }
 
-    pub fn delete(&mut self, id: I) -> Option<()> {
+    pub fn delete_by_id(&mut self, id: I) -> Option<()> {
         for element in self.elements.iter_mut() {
             if element.0 == id {
                 element.2 = true;
@@ -63,13 +63,22 @@ impl<I: PartialEq + Eq + Hash + Clone + PartialOrd, V: PartialEq + Eq + Hash + C
         }
         None
     }
+    
+    pub fn get_by_id(&self, id: I) -> Option<Element<I, V>> {
+        for element in self.elements.iter() {
+            if element.0 == id {
+                return Some(element.clone());
+            }
+        }
+        None
+    }
 
-    pub fn get(&self, idx: usize) -> Option<V> {
+    pub fn get_by_idx(&self, idx: usize) -> Option<Element<I, V>> {
         let mut count = 0;
         for element in self.elements.iter() {
             if !element.2 {
                 if count == idx {
-                    return Some(element.1.clone());
+                    return Some(element.clone());
                 }
                 count += 1;
             }
@@ -77,6 +86,20 @@ impl<I: PartialEq + Eq + Hash + Clone + PartialOrd, V: PartialEq + Eq + Hash + C
         None
     }
 
+    pub fn delete_by_idx(&mut self, idx: usize) -> Option<()> {
+        let mut count = 0;
+        for element in self.elements.iter_mut() {
+            if !element.2 {
+                if count == idx {
+                    element.2 = true;
+                    return Some(());
+                }
+                count += 1;
+            }
+        }
+        None
+    }
+    
     pub fn get_list(&self) -> Vec<V> {
         self.elements.iter().filter(|e| !e.2).map(|e| e.1.clone()).collect()
     }
@@ -97,9 +120,9 @@ mod tests {
     #[test]
     fn test_insert() {
         let mut list = OrderedList::new();
-        list.insert(1, "a", None);
-        list.insert(2, "b", Some(1));
-        list.insert(3, "c", Some(2));
+        list.insert_by_id(1, "a", None);
+        list.insert_by_id(2, "b", Some(1));
+        list.insert_by_id(3, "c", Some(2));
         
         assert_eq!(list.get_list(), vec!["a", "b", "c"]);
     }
@@ -107,33 +130,33 @@ mod tests {
     #[test]
     fn test_delete() {
         let mut list = OrderedList::new();
-        list.insert(1, "a", None);
-        list.insert(2, "b", Some(1));
-        list.insert(3, "c", Some(2));
-        list.delete(2);
+        list.insert_by_id(1, "a", None);
+        list.insert_by_id(2, "b", Some(1));
+        list.insert_by_id(3, "c", Some(2));
+        list.delete_by_id(2);
         assert_eq!(list.get_list(), vec!["a", "c"]);
     }
 
     #[test]
     fn test_insert_at_the_same_position() {
         let mut list = OrderedList::new();
-        list.insert(1, "a", None);
-        list.insert(2, "b", Some(1));
-        list.insert(3, "c", Some(1));
+        list.insert_by_id(1, "a", None);
+        list.insert_by_id(2, "b", Some(1));
+        list.insert_by_id(3, "c", Some(1));
         assert_eq!(list.get_list(), vec!["a", "c", "b"]);
     }
 
     #[test]
     fn concurrent_insert_commute() {
         let mut list = OrderedList::new();
-        list.insert(1, "a", None);
-        list.insert(2, "b", Some(1));
-        list.insert(3, "c", Some(1));
+        list.insert_by_id(1, "a", None);
+        list.insert_by_id(2, "b", Some(1));
+        list.insert_by_id(3, "c", Some(1));
         
         let mut list2 = OrderedList::new();
-        list2.insert(1, "a", None);
-        list2.insert(3, "c", Some(1));
-        list2.insert(2, "b", Some(1));
+        list2.insert_by_id(1, "a", None);
+        list2.insert_by_id(3, "c", Some(1));
+        list2.insert_by_id(2, "b", Some(1));
 
         assert_eq!(list.get_list(), list2.get_list());
     }
@@ -141,15 +164,15 @@ mod tests {
     #[test]
     fn concurrent_delete_commute() {
         let mut list = OrderedList::new();
-        list.insert(1, "a", None);
-        list.insert(2, "b", Some(1));
+        list.insert_by_id(1, "a", None);
+        list.insert_by_id(2, "b", Some(1));
 
         let mut list2 = list.clone();
 
-        list.delete(1);
-        list.delete(2);
-        list2.delete(2);
-        list2.delete(1);
+        list.delete_by_id(1);
+        list.delete_by_id(2);
+        list2.delete_by_id(2);
+        list2.delete_by_id(1);
 
         assert_eq!(list.get_list(), list2.get_list());
     }
@@ -157,17 +180,17 @@ mod tests {
     #[test]
     fn concurrent_insert_delete_commute() {
         let mut list = OrderedList::new();
-        list.insert(1, "a", None);
-        list.insert(2, "b", Some(1));
-        list.insert(3, "c", Some(2));
+        list.insert_by_id(1, "a", None);
+        list.insert_by_id(2, "b", Some(1));
+        list.insert_by_id(3, "c", Some(2));
 
         let mut list2 = list.clone();
 
-        list.insert(2, "d", Some(1));
-        list.delete(1);
+        list.insert_by_id(2, "d", Some(1));
+        list.delete_by_id(1);
 
-        list2.delete(1);
-        list2.insert(2, "d", Some(1));
+        list2.delete_by_id(1);
+        list2.insert_by_id(2, "d", Some(1));
 
 
         assert_eq!(list.get_list(), list2.get_list());
---
+++ src/crdts/rga.rs
@@ -0,0 +1,99 @@
+use crate::crdts::crdt::CRDT;
+use crate::crdts::ordered_list::OrderedList;
+use std::hash::Hash;
+
+pub enum RGAOp<I: PartialEq + Eq + Hash + Clone, V: PartialEq + Eq + Hash + Clone> {
+    Insert(I, V, Option<I>),
+    Delete(I),
+}
+
+pub struct RGA<I: PartialEq + Eq + Hash + Clone + PartialOrd, V: PartialEq + Eq + Hash + Clone> {
+    elements: OrderedList<I, V>,
+}
+
+impl<I: PartialEq + Eq + Hash + Clone + PartialOrd, V: PartialEq + Eq + Hash + Clone> RGA<I, V> {
+    pub fn new() -> Self {
+        RGA {
+            elements: OrderedList::new(),
+        }
+    }
+
+    pub fn get(&self, idx: usize) -> Option<V> {
+        self.elements.get_by_idx(idx).map(|(_, v, _)| v)
+    }
+
+    pub fn get_list(&self) -> Vec<V> {
+        self.elements.get_list()
+    }
+
+    pub fn insert(&mut self, idx: usize, value: V, elem_id: I) -> Option<RGAOp<I, V>> {
+        if idx == 0 {
+            Some(RGAOp::Insert(elem_id, value, None))
+        } else {
+            let prev = self.elements.get_by_idx(idx - 1);
+            if let Some((id, _, _)) = prev {
+                Some(RGAOp::Insert(elem_id, value, Some(id.clone())))
+            } else {
+                None
+            }
+        }
+    }
+    
+    pub fn delete(&mut self, idx: usize) -> Option<RGAOp<I, V>> {
+        let elem = self.elements.get_by_idx(idx);
+        if let Some((id, _, _)) = elem {
+            Some(RGAOp::Delete(id.clone()))
+        } else {
+            None
+        }
+    }
+}
+
+impl<I: PartialEq + Eq + Hash + Clone + PartialOrd, V: PartialEq + Eq + Hash + Clone> CRDT<RGAOp<I, V>> for RGA<I, V> {
+    fn interpret_op(&mut self, op: &RGAOp<I, V>) {
+        match op {
+            RGAOp::Insert(id, value, after) => self.elements.insert_by_id(id.clone(), value.clone(), after.clone()),
+            RGAOp::Delete(id) => self.elements.delete_by_id(id.clone()),
+        };
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_insert() {
+        let mut rga = RGA::new();
+        let insert_op = rga.insert(0, "a", 0).unwrap();
+        rga.interpret_op(&insert_op);
+        let insert_op = rga.insert(1, "b", 1).unwrap();
+        rga.interpret_op(&insert_op);
+        let insert_op = rga.insert(2, "c", 2).unwrap();
+        rga.interpret_op(&insert_op);
+        let insert_op = rga.insert(1, "d", 3).unwrap();
+        rga.interpret_op(&insert_op);
+        assert_eq!(rga.get_list(), vec!["a", "d", "b", "c"]);
+    }
+    
+    #[test]
+    fn test_delete() {
+        let mut rga = RGA::new();
+        let insert_op = rga.insert(0, "a", 0).unwrap();
+        rga.interpret_op(&insert_op);
+        let insert_op = rga.insert(1, "b", 1).unwrap();
+        rga.interpret_op(&insert_op);
+        let insert_op = rga.insert(2, "c", 2).unwrap();
+        rga.interpret_op(&insert_op);
+        let delete_op = rga.delete(1).unwrap();
+        rga.interpret_op(&delete_op);
+        assert_eq!(rga.get_list(), vec!["a", "c"]);
+    }
+    
+    #[test]
+    fn test_insert_invalid() {
+        let mut rga = RGA::new();
+        let insert_op = rga.insert(1, "a", 0);
+        assert!(insert_op.is_none());
+    }
+}
